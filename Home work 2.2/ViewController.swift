f1ebc5740284ef24b93180adfff7178c84781570
Dmitry <borodulkin_d@mail.ru>
2021-12-12 18:12:36 +0300

    Created a view, fixed it on the screen and rounded the edges.

--- Home work 2.2/ViewController.swift
+++ Home work 2.2/ViewController.swift
@@ -8,10 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mixView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        mixView.layer.cornerRadius = 10
     }
 
 
