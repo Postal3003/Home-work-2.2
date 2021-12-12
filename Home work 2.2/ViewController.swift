de16f10bd055a0feb22d54965901657e4f7bb1e1
Dmitry <borodulkin_d@mail.ru>
2021-12-12 23:17:22 +0300

    I created three sliders, put them on the stack view and pinned them to the screen.

--- Home work 2.2/ViewController.swift
+++ Home work 2.2/ViewController.swift
@@ -9,15 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var mixView: UIView!
+    @IBOutlet weak var colorView: UIView!
     
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        mixView.layer.cornerRadius = 10
+        // Do any additional setup after loading the view,typically from a nib.
+        colorView.layer.cornerRadius = 10
     }
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func changeColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat (greenSlider.value), blue: CGFloat (blueSlider.value), alpha: 1)
+    }
+    @IBAction func rgbSliderChanged(_ sender: Any) {
+        changeColor()
+    }
+    
 
 }
 
